622628888ad934420abbcc5caf40c17d3624cce0
AleBelwolf <jose_20alejandro14@hotmail.com>
2023-08-23 13:43:46 -0300

    fim do menu

--- Curriculo-Jose-Alejandro/script.js
+++ Curriculo-Jose-Alejandro/script.js
@@ -1,12 +1,14 @@
 function burguer() {
     let menu = document.querySelector('#menu')
+    let itensMenu = document.querySelector('#itensMenu')
 
     /* Para fazer ativar a rotação e o X */
     let menuBtn = document.getElementById('burguer');
     menuBtn.classList.toggle('active');
  
     if (menu.style.display == 'block') {
-        menu.style.display = 'none'     
+        menu.style.display = 'none'   
+          
     }
     else {
         menu.style.display = 'block'  
